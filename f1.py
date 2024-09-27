404e0075baf59d187dc6bab9c84c8c5825c5eac5
Gaurav Taxali <61114467+gaurav16-byte@users.noreply.github.com>
2024-09-27 10:27:00 +0530

    Added driver data comparison

--- f1.py
+++ f1.py
@@ -1,17 +1,24 @@
 from urllib.request import urlopen
 import json
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 import seaborn as sns
+from time import sleep
+import numpy as np
+from scipy.interpolate import interp1d
 
 def mainn():    #TO HAVE THE TRACKS AND COUNTRY ALONG WITH DRIVER DETAILS
     driver_acronyms_2024 = ['VER', 'PER', 'LEC', 'SAI', 'HAM', 'RUS', 'ALO', 'STR', 'NOR', 'PIA', 'GAS', 'OCO', 'BOT', 'ZHO', 'MAG', 'HUL', 'TSU', 'DEV', 'ALB', 'SAR', 'COL', 'BEA']
     driver_numbers_2024 = [1, 11, 16, 55, 44, 63, 14, 18, 4, 81, 10, 31, 77, 24, 20, 27, 22, 21, 23, 2, 50, 34]
     drivers = dict(zip(driver_acronyms_2024, driver_numbers_2024))
+    drivers1 = dict(zip(driver_numbers_2024, driver_acronyms_2024))
     
-    countries = ['Bahrain', 'Saudi Arabia', 'Australia', 'Azerbaijan', 'United States', 'Italy', 'Monaco', 'Spain', 'Canada', 'Austria', 'United Kingdom', 'Hungary', 'Belgium', 'Netherlands', 'Singapore', 'Japan', 'Qatar', 'Mexico', 'Brazil', 'United States', 'Abu Dhabi', 'China', 'United States', 'Italy']
+    countries = ['Bahrain', 'Saudi Arabia', 'Australia', 'Azerbaijan', 'United States', 'Italy', 'Monaco', 'Spain', 'Canada', 'Austria', 'United Kingdom', 'Hungary', 'Belgium', 'Netherlands', 'Singapore', 'Japan', 'Qatar', 'Mexico', 'Brazil', 'United States', 'United Arab Emirates', 'China', 'United States', 'Italy']
     circuits = ['','','','','Miami','Imola','','','','','','','','','','','','','','Austin','','','Las Vegas', 'Monza']
     laps = [57, 50, 58, 51, 57, 63, 78, 66, 70, 71, 52, 70, 44, 72, 62, 53, 57, 71, 71, 56, 58, 56, 50, 53]
+    return drivers, drivers1
+
+all_drivers, driver_nos = mainn()
 
 def solve_time(date, gmt):  #TO DISPLAY TIME IN THE LOCAL TIME OF THE COUNTRY
     dat = date[:date.find('T')]
@@ -21,9 +28,10 @@ def solve_time(date, gmt):  #TO DISPLAY TIME IN THE LOCAL TIME OF THE COUNTRY
     utc_datetime = datetime.strptime(f"{dat} {time}", "%Y-%m-%d %H:%M:%S")
     gmt_offset = timedelta(hours=hour, minutes=minute)
     local_datetime = utc_datetime + gmt_offset
-    return local_datetime
+    print(f'Local Time: {local_datetime}')
 
 def namesss(country, year, ckt = None): #TO GET PROPER NAME OF THE GRAND PRIX
+    sleep(1)
     response = urlopen(f'https://api.openf1.org/v1/meetings?year={year}&country_name={country}')
     data = json.loads(response.read().decode('utf-8'))
     if len(data) == 1:
@@ -34,8 +42,9 @@ def namesss(country, year, ckt = None): #TO GET PROPER NAME OF THE GRAND PRIX
                 data = data[i]
     return data["meeting_official_name"]
 
-def important_values(country, session_type, year, ckt = None):  #TO GET THE SESSION, MEETING KEYS, SHORT CIRCUIT NAME, AND COUNTRY
+def important_values(country, year, ckt = None, session_type = 'Race'):  #TO GET THE SESSION, MEETING KEYS, SHORT CIRCUIT NAME, AND COUNTRY
     response = urlopen(f'https://api.openf1.org/v1/sessions?country_name={country}&session_name={session_type}&year={year}')
+    sleep(1)
     data = json.loads(response.read().decode('utf-8'))
     if len(data) == 1:
         data = data[0]
@@ -47,17 +56,24 @@ def important_values(country, session_type, year, ckt = None):  #TO GET THE SESS
     meeting_key = data['meeting_key']
     ckt_short = data['circuit_short_name']
     country_name = data['country_name']
+    print(namesss(country_name.replace(' ','+'), year, ckt))
     start_date = data['date_start']
     gmt_offset = data['gmt_offset']
-    local_time = solve_time(start_date, gmt_offset)
+    solve_time(start_date, gmt_offset)
     return session_key, meeting_key, ckt_short, country_name
 
-session_key, meeting_key, ckt_short, country_name = important_values('Brazil', 'Race', 2023)
+session_key, meeting_key, ckt_short, country_name = important_values('United+States', 2023, 'Las Vegas')
 
 def cars_data(driver_number, session_key = session_key):
+    sleep(1)
     response = urlopen(f'https://api.openf1.org/v1/car_data?driver_number={driver_number}&session_key={session_key}&speed>=1')  #SPEED AND OTHER CAR DETAILS
     data = json.loads(response.read().decode('utf-8'))
-    rpm = speed = gear = throttle = brake = drs = []
+    rpm = []
+    speed = []
+    gear = []
+    throttle = []
+    brake = []
+    drs = []
     for i in data:
         rpm.append(i['rpm'])
         speed.append(i['speed'])
@@ -65,16 +81,30 @@ def cars_data(driver_number, session_key = session_key):
         throttle.append(i['throttle'])
         brake.append(i['brake'])
         drs.append(i['drs'])
+
     return rpm, speed, gear, throttle, brake, drs
 
-def drivers_info(car_no, session_key = session_key):    #GET TEAM COLOR AND DRIVERS SHORT NAME
-    response = urlopen(f'https://api.openf1.org/v1/drivers?driver_number={car_no}&session_key={session_key}')
-    data = json.loads(response.read().decode('utf-8'))[0]
-    short = data['name_acronym']
-    team_color = data['team_colour']
-    return short, team_color
+
+ 
+def compare_drivers(d1, d2, session_key = session_key, driver_nos = driver_nos):
+    color1 = '#3671C6'
+    color2 = '#FF8700'
+    rpm1, speed1, gear1, throttle1, brake1, drs1 = cars_data(d1)
+    rpm2, speed2, gear2, throttle2, brake2, drs2 = cars_data(d2)
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(speed1, color=color1, label=driver_nos[d1])
+    plt.plot(speed2, color=color2, label=driver_nos[d2])
+    plt.title("Speed Comparison of Drivers")
+    plt.xlabel("Time (or Lap Data Points)")
+    plt.ylabel("Speed (km/h)")
+    plt.legend(loc="upper right")
+    plt.show()
+
+compare_drivers(1, 44)
 
 def laps_info(driver, lap_number, session_key = session_key):   #ADDED LAP TIMES AND MINI SECTORS
+    sleep(1)
     try:
         response = urlopen(f'https://api.openf1.org/v1/laps?session_key={session_key}&driver_number={driver}&lap_number={lap_number}')
         data = json.loads(response.read().decode('utf-8'))
@@ -93,15 +123,16 @@ def laps_info(driver, lap_number, session_key = session_key):   #ADDED LAP TIMES
     seg1 = data[0]['segments_sector_1']
     seg2 = data[0]['segments_sector_2']
     seg3 = data[0]['segments_sector_3']
-    return speed_trap, duration, out_lap, sec1, sec2, sec3, seg1, seg2, seg3, started
+    time_obj = datetime.fromisoformat(started)
+    seconds_to_add = duration
+    new_time = time_obj + timedelta(seconds=seconds_to_add)
+    new_time_string = new_time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+    return started, new_time_string
 
-speed_trap, duration, out_lap, sec1, sec2, sec3, seg1, seg2, seg3, started = laps_info(1,25)
-time_obj = datetime.fromisoformat(started)
-seconds_to_add = duration
-new_time = time_obj + timedelta(seconds=seconds_to_add)
-new_time_string = new_time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+started, new_time_string = laps_info(1, 25)
 
 def locations(driver, start, end, session_key = session_key):   #ADDED TRACKERS FOR CARS
+    sleep(1)
     response = urlopen(f'https://api.openf1.org/v1/location?session_key={session_key}&driver_number={driver}&date>{start}&date<{end}')
     data = json.loads(response.read().decode('utf-8'))
     xx = []
@@ -109,7 +140,7 @@ def locations(driver, start, end, session_key = session_key):   #ADDED TRACKERS 
     for i in data:
         xx.append(i['x'])
         yy.append(i['y'])
-    #'''
+    '''
     plt.figure(figsize=(10, 8))
     plt.plot(xx, yy, color='blue', label='Car Path')
     plt.xlabel('X Coordinates')
@@ -118,11 +149,12 @@ def locations(driver, start, end, session_key = session_key):   #ADDED TRACKERS 
     plt.grid(True)
     plt.legend()
     plt.show()
-    #'''
+    '''
 
 #locations(1, started, new_time_string)
 
-def tyres(session_key = session_key):   #STINTS FOR ALL DRIVERS USE A FOR LOOP FOR SPECIFIC DRIVER DATA OR 
+def tyres(session_key = session_key):   #STINTS FOR ALL DRIVERS USE A FOR LOOP FOR SPECIFIC DRIVER DATA OR
+    sleep(1) 
     response = urlopen(f'https://api.openf1.org/v1/stints?session_key={session_key}&tyre_age_at_start>=0')
     data = json.loads(response.read().decode('utf-8'))
     compound = data[0]['compound']
@@ -131,25 +163,12 @@ def tyres(session_key = session_key):   #STINTS FOR ALL DRIVERS USE A FOR LOOP F
     end_lap = data[0]['lap_end']
 
 def radios(driver, session_key = session_key):  #WITH COMMENTS CAN LISTEN TO RADIOS ONE BY ONE
+    sleep(1)
     response = urlopen(f'https://api.openf1.org/v1/team_radio?session_key={session_key}&driver_number={driver}')
     data = json.loads(response.read().decode('utf-8'))
-    '''
-    import pygame
-    import requests
-    from io import BytesIO
-    audios = []
-    for i in x:
-        audios.append(i['recording_url'])
-    def listen(link):
-        pygame.mixer.init()
-        response = requests.get(link)
-        audio_file = BytesIO(response.content)
-        pygame.mixer.music.load(audio_file)
-        pygame.mixer.music.play()
-    listen(audios[0])
-    '''
 
 def temps(meeting_key = meeting_key):
+    sleep(1)
     response = urlopen(f'https://api.openf1.org/v1/weather?meeting_key={meeting_key}&wind_direction>=0&track_temperature>=1')
     data = json.loads(response.read().decode('utf-8'))
     air = []
@@ -198,4 +217,3 @@ def temps(meeting_key = meeting_key):
         ax.axvline(0, color=font_color, lw=1.0)  # Vertical axis (Y-axis)
     plt.tight_layout(pad=2.0)
     plt.show()
-temps()    
